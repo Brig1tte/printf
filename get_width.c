5f7d38b0418c12982ce5b8488c4377d686694055
Brig1tte <korantengbrigitte96@gmail.com>
2023-05-30 07:50:43 -0700

    Printf Group Work

--- get_width.c
+++ get_width.c
@@ -1,28 +1,34 @@
 #include "main.h"
 
 /**
- * get_width - Calculaes the width for printing
+ * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed
+ * @a: List of arguments to be printed
  * @list: List of arguments
  *
  * Return: Width
  */
-int get_width(const char *format, int *i, va_list list)
+int get_width(const char *format, int *a, va_list list)
 {
 	int curr_a;
 	int width = 0;
 
-	for (curr_i = *a + 1; format[curr_a] != '\0'; curr_a++)
+	for (curr_a = *a + 1; format[curr_a] != '\0'; curr_a++)
 	{
 		if (is_digit(format[curr_a]))
 		{
 			width *= 10;
 			width += format[curr_a] - '0';
 		}
+		else if (format[curr_a] == '*')
+		{
+			curr_a++;
+			width = va_arg(list, int);
+			break;
+		}
 		else
 			break;
 	}
-	*i = curr_a - 1;
+	*a = curr_a - 1;
 	return (width);
 }
